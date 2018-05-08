1b1d7aeedaf9ee891ebc8e5bf17ba15b5e0bb91c
Zach Deibert <zachdeibert@gmail.com>
2018-05-08 10:04:08 -0500

    Removed "Scene 1"

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
 	public void PlayGame() {
-		SceneManager.LoadScene ("Scene1");
+		SceneManager.LoadScene ("Low Poly");
 	}
 
 	public void QuitGame() {
